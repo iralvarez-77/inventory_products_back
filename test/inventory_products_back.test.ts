daec2b1ed3a985f31ae3141b6b06962a3fb4fb66
Irainelly Alvarez <irainelly.alvarez@comafi.com.ar>
2026-09-12 15:42:08 -0400

    Initial commit

---
+++ test/inventory_products_back.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as InventoryProductsBack from '../lib/inventory_products_back-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/inventory_products_back-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new InventoryProductsBack.InventoryProductsBackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
